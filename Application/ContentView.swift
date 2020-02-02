261d128d501f211ad149ef5013f74d240d7bbe0c
Gábor Librecz <librecz.gabor@gmail.com>
2020-02-02 15:39:14 +0100

    Remove TabbedView

--- Application/ContentView.swift
+++ Application/ContentView.swift
@@ -1,67 +1,168 @@
 import About
 import ComposableArchitecture
+import Rules
+import Settings
 import StyleGuide
 import SwiftFormatConfiguration
 import SwiftUI
-import TabbedView
 
 public struct AppState {
   public var configuration: Configuration
   public var selectedTab: Int
-
-  public init(configuration: Configuration, selectedTab: Int) {
-    self.configuration = configuration
-    self.selectedTab = selectedTab
-  }
 }
 
 enum AppAction {
-  case tabbedView(TabbedViewAction)
+  case settingsView(SettingsViewAction)
 
-  var tabbedView: TabbedViewAction? {
+  var settingsView: SettingsViewAction? {
     get {
-      guard case let .tabbedView(value) = self else { return nil }
+      guard case let .settingsView(value) = self else { return nil }
       return value
     }
     set {
-      guard case .tabbedView = self, let newValue = newValue else { return }
-      self = .tabbedView(newValue)
+      guard case .settingsView = self, let newValue = newValue else { return }
+      self = .settingsView(newValue)
+    }
+  }
+
+  case rulesView(RulesViewAction)
+
+  var rulesView: RulesViewAction? {
+    get {
+      guard case let .rulesView(value) = self else { return nil }
+      return value
+    }
+    set {
+      guard case .rulesView = self, let newValue = newValue else { return }
+      self = .rulesView(newValue)
+    }
+  }
+
+  case tabView(TabViewAction)
+
+  var tabView: TabViewAction? {
+    get {
+      guard case let .tabView(value) = self else { return nil }
+      return value
+    }
+    set {
+      guard case .tabView = self, let newValue = newValue else { return }
+      self = .tabView(newValue)
     }
   }
 }
 
 extension AppState {
-  var tabbedView: TabbedViewState {
+  var settingsView: SettingsViewState {
     get {
-      TabbedViewState(
-        configuration: self.configuration,
-        selectedTab: self.selectedTab
+      SettingsViewState(
+        maximumBlankLines: self.configuration.maximumBlankLines,
+        lineLength: self.configuration.lineLength,
+        tabWidth: self.configuration.tabWidth,
+        indentation: self.configuration.indentation,
+        respectsExistingLineBreaks: self.configuration
+          .respectsExistingLineBreaks,
+        lineBreakBeforeControlFlowKeywords: self.configuration
+          .lineBreakBeforeControlFlowKeywords,
+        lineBreakBeforeEachArgument: self.configuration
+          .lineBreakBeforeEachArgument,
+        lineBreakBeforeEachGenericRequirement: self.configuration
+          .lineBreakBeforeEachGenericRequirement,
+        prioritizeKeepingFunctionOutputTogether: self.configuration
+          .prioritizeKeepingFunctionOutputTogether,
+        indentConditionalCompilationBlocks: self.configuration
+          .indentConditionalCompilationBlocks,
+        lineBreakAroundMultilineExpressionChainComponents: self.configuration
+          .lineBreakAroundMultilineExpressionChainComponents
       )
     }
     set {
-      self.configuration = newValue.configuration
-      self.selectedTab = newValue.selectedTab
+      self.configuration.maximumBlankLines = newValue.maximumBlankLines
+      self.configuration.lineLength = newValue.lineLength
+      self.configuration.tabWidth = newValue.tabWidth
+      self.configuration.indentation = newValue.indentation
+      self.configuration.respectsExistingLineBreaks =
+        newValue.respectsExistingLineBreaks
+      self.configuration.lineBreakBeforeControlFlowKeywords =
+        newValue.lineBreakBeforeControlFlowKeywords
+      self.configuration.lineBreakBeforeEachArgument =
+        newValue.lineBreakBeforeEachArgument
+      self.configuration.lineBreakBeforeEachGenericRequirement =
+        newValue.lineBreakBeforeEachGenericRequirement
+      self.configuration.prioritizeKeepingFunctionOutputTogether =
+        newValue.prioritizeKeepingFunctionOutputTogether
+      self.configuration.indentConditionalCompilationBlocks =
+        newValue.indentConditionalCompilationBlocks
+      self.configuration.lineBreakAroundMultilineExpressionChainComponents =
+        newValue.lineBreakAroundMultilineExpressionChainComponents
     }
+  }
+
+  var rulesView: RulesViewState {
+    get { RulesViewState(rules: self.configuration.rules) }
+    set { self.configuration.rules = newValue.rules }
+  }
+
+  var tabView: TabViewState {
+    get { TabViewState(selectedTab: self.selectedTab) }
+    set { self.selectedTab = newValue.selectedTab }
+  }
+}
+
+enum TabViewAction: Equatable { case tabSelected(Int) }
+
+struct TabViewState { var selectedTab: Int }
+
+func tabViewReducer(state: inout TabViewState, action: TabViewAction)
+  -> [Effect<TabViewAction>]
+{
+  switch action {
+  case .tabSelected(let selectedTab):
+    state.selectedTab = selectedTab
+    return []
   }
 }
 
 let appReducer = combine(
   pullback(
-    tabbedViewReducer,
-    value: \AppState.tabbedView,
-    action: \AppAction.tabbedView
-  )
+    settingsViewReducer,
+    value: \AppState.settingsView,
+    action: \AppAction.settingsView
+  ),
+  pullback(
+    rulesViewReducer,
+    value: \AppState.rulesView,
+    action: \AppAction.rulesView
+  ),
+  pullback(tabViewReducer, value: \AppState.tabView, action: \AppAction.tabView)
 )
 
 struct ContentView: View {
   @ObservedObject var store: Store<AppState, AppAction>
 
   var body: some View {
-    TabbedView(
-      store: self.store.view(
-        value: { $0.tabbedView },
-        action: { .tabbedView($0) }
+    TabView(
+      selection: Binding(
+        get: { self.store.value.selectedTab },
+        set: { self.store.send(.tabView(.tabSelected($0))) }
       )
-    )
+    ) {
+      SettingsView(
+        store: self.store.view(
+          value: { $0.settingsView },
+          action: { .settingsView($0) }
+        )
+      )
+      .padding().tabItem { Text("Formatting") }.tag(0)
+      RulesView(
+        store: self.store.view(
+          value: { $0.rulesView },
+          action: { .rulesView($0) }
+        )
+      )
+      .padding().tabItem { Text("Rules") }.tag(1)
+      AboutView().padding().tabItem { Text("About") }.tag(2)
+    }
+    .modifier(PrimaryTabViewStyle())
   }
 }
