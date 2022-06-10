ff68b151ad7966d4448ae70b9f50715d19beef3d
Gábor Librecz <kuglee@gmail.com>
2022-06-10 22:50:33 +0200

    Remove unused function

--- Sources/Utility/Utility.swift
+++ Sources/Utility/Utility.swift
@@ -48,5 +48,3 @@ private struct TooltipView: NSViewRepresentable {
     nsView.toolTip = self.toolTip
   }
 }
-
-public func with<A, B>(_ a: A, _ f: (A) throws -> B) rethrows -> B { return try f(a) }
