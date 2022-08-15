575d4332a5cc7779bae316babd1b433a8744964a
Gábor Librecz <kuglee@gmail.com>
2022-08-15 16:36:48 +0200

    Rename alignments

--- Sources/StyleGuide/Alignments.swift
+++ Sources/StyleGuide/Alignments.swift
@@ -1,19 +1,19 @@
 import SwiftUI
 
 extension HorizontalAlignment {
-  private enum TrailingLabel: AlignmentID {
+  private enum TrailingLabelAlignment: AlignmentID {
     static func defaultValue(in context: ViewDimensions) -> CGFloat { context[.trailing] }
   }
 
-  public static let trailingLabel = HorizontalAlignment(TrailingLabel.self)
+  public static let trailingLabel = HorizontalAlignment(TrailingLabelAlignment.self)
 }
 
 extension VerticalAlignment {
-  private enum CenterNonSiblings: AlignmentID {
+  private enum CenterNonSiblingsAlignment: AlignmentID {
     static func defaultValue(in context: ViewDimensions) -> CGFloat {
       context[VerticalAlignment.center]
     }
   }
 
-  public static let centerNonSiblings = VerticalAlignment(CenterNonSiblings.self)
+  public static let centerNonSiblings = VerticalAlignment(CenterNonSiblingsAlignment.self)
 }
