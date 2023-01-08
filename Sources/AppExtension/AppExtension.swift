a835853dbea3adce991abedbabab52059331a883
Gábor Librecz <kuglee@gmail.com>
2023-01-08 14:33:21 +0100

    Use SwiftFormat package for formatting instead of the swift-format binary (with custom swift-format fork to handle invalid swift source)

--- Sources/AppExtension/AppExtension.swift
+++ Sources/AppExtension/AppExtension.swift
@@ -1,7 +1,6 @@
-import AppConstants
 import AppKit
 import AppUserDefaults
-import SwiftFormatterServiceProtocol
+import SwiftFormat
 import XcodeKit
 import os.log
 
@@ -33,31 +32,26 @@ class FormatSourceCommand: NSObject, XCSourceEditorCommand {
         .contains(invocation.buffer.contentUTI)
     else { return completionHandler(nil) }
 
-    let serviceConnection = getServiceConnection()
-    serviceConnection.resume()
-
-    let service = getService(connection: serviceConnection)
-
     let source =
       AppUserDefaults.live.getShouldTrimTrailingWhitespace()
       ? invocation.buffer.completeBufferTrimmed : invocation.buffer.completeBuffer
+    var formattedSource = ""
 
-    service.format(source: source) { formattedSource, error in
-      defer { serviceConnection.invalidate() }
-
-      if let _ = error { return completionHandler(nil) }
-
-      guard let formattedSource = formattedSource else { return completionHandler(nil) }
-
-      if source == formattedSource { return completionHandler(nil) }
-
-      let previousSelection = invocation.buffer.selections[0] as! XCSourceTextRange
-      invocation.buffer.selections.removeAllObjects()
-      invocation.buffer.completeBuffer = formattedSource
-      invocation.buffer.selections.add(previousSelection)
-
-      return completionHandler(nil)
+    let formatter = SwiftFormatter(
+      configuration: AppUserDefaults.live.getConfigurationWrapper().toConfiguration()
+    )
+    do { try formatter.format(source: source, assumingFileURL: nil, to: &formattedSource) } catch {
+      os_log("Unable to format source: %{public}@", error.localizedDescription)
     }
+
+    if formattedSource.isEmpty || source == formattedSource { return completionHandler(nil) }
+
+    let previousSelection = invocation.buffer.selections[0] as! XCSourceTextRange
+    invocation.buffer.selections.removeAllObjects()
+    invocation.buffer.completeBuffer = formattedSource
+    invocation.buffer.selections.add(previousSelection)
+
+    return completionHandler(nil)
   }
 }
 
@@ -86,17 +80,4 @@ extension StringProtocol {
 
     return view
   }
-}
-
-func getServiceConnection() -> NSXPCConnection {
-  let connection = NSXPCConnection(serviceName: AppConstants.xpcServiceName)
-  connection.remoteObjectInterface = NSXPCInterface(with: SwiftFormatterServiceProtocol.self)
-
-  return connection
-}
-
-func getService(connection: NSXPCConnection) -> SwiftFormatterServiceProtocol {
-  return (connection.remoteObjectProxy as! NSXPCProxyCreating)
-    .remoteObjectProxyWithErrorHandler { error in os_log("%{public}@", error.localizedDescription) }
-    as! SwiftFormatterServiceProtocol
 }
